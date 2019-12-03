3c2aa2408f5dfbca8f9c70d2fd05661e6294d62f
undefined <undefined>
2019-12-03 18:48:46 +0800

    init vuex-loading

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const createLoadingPlugin = ({ namespace: NAMESPACE = "loading" } = {}) => {
+  const SHOW = "@@ANTDV_LOADING/SHOW";
+  const HIDE = "@@ANTDV_LOADING/HIDE";
+  return store => {
+    if (store.state[NAMESPACE]) {
+      throw new Error(
+        `createLoadingPlugin: ${NAMESPACE} exited in current store`
+      );
+    }
+    store.registerModule(NAMESPACE, {
+      namespaced: true,
+      state: {
+        global: false,
+        models: {},
+        effects: {}
+      },
+      mutations: {
+        [SHOW]: (state, { payload: { actionType } }) => {
+          state.global = true;
+          const _namespace = actionType.split("/")[0];
+          state.models = { ...state.models, [_namespace]: true };
+          state.effects = { ...state.effects, [actionType]: true };
+        },
+        [HIDE]: (state, { payload: { actionType } }) => {
+          state.global = false;
+          const _namespace = actionType.split("/")[0];
+          state.models = { ...state.models, [_namespace]: false };
+          state.effects = { ...state.effects, [actionType]: false };
+        }
+      }
+    });
+    store.subscribeAction({
+      before: action => {
+        store.commit(
+          {
+            type: `${NAMESPACE}/${SHOW}`,
+            payload: { actionType: action.type }
+          },
+          { root: true }
+        );
+      },
+      after: action => {
+        store.commit(
+          {
+            type: `${NAMESPACE}/${HIDE}`,
+            payload: { actionType: action.type }
+          },
+          { root: true }
+        );
+      }
+    });
+  };
+};
+
+export default createLoadingPlugin;
